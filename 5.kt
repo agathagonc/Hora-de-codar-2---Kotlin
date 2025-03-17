b904828ae99cdcea0e79fc22e491489f09e8b850
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-17 20:52:09 -0300

    Add files via upload

---
+++ 5.kt
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    // Exercicio 5
+
+    println("Digite um número: ")
+    var n1 = readln().toInt()
+
+    println("Digite um número: ")
+    var n2 = readln().toInt()
+
+    println("Digite um número: ")
+    var n3 = readln().toInt()
+
+    println("Digite um número: ")
+    var n4 = readln().toInt()
+
+    println("Digite um número: ")
+    var n5 = readln().toInt()
+
+    println("Digite um número: ")
+    var n6 = readln().toInt()
+
+    println("A média aritmética será: " + (n1 + n2 + n3 + n4 + n5 + n6)/2)
+
+}
