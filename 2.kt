00102a866cf0dd94628c1451dab37cbc8211840e
Agatha Gonçalves <agathagoncalves002@gmail.com>
2025-03-17 20:47:28 -0300

    Add files via upload

---
+++ 2.kt
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+     //Exercicio 2
+
+    print("Digite um número: ")
+    var n1 = readln().toInt()
+
+    if(n1 > 0){
+        print("O número é positivo")
+    }else if (n1 < 0){
+        print("O número é negativo")
+    }else if (n1 == 0){
+        print("O número é zero")
+    }
+
+}
